9f62baaefcd28c215bb3a6bae788ccddbcbd37d5
Ola Połaniecka <97olka@gmail.com>
2023-11-17 11:21:32 +0100

    changes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startColor = document.querySelector('[data-start]');
+const stopColor = document.querySelector('[data-stop]');
+let interval = null;
+
+startColor.addEventListener('click', () => {
+  startColor.disabled = true;
+  stopColor.disabled = false;
+  interval = setInterval(() => {
+    startColor.parentNode.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+stopColor.addEventListener('click', () => {
+  startColor.disabled = false;
+  stopColor.disabled = true;
+  clearInterval(interval);
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,61 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMinutes = document.querySelector('[data-minutes]');
+const timerSeconds = document.querySelector('[data-seconds]');
+
+let timer;
+
+let date = new Date();
+let newDate = new Date();
+
+const start = document.querySelector('button');
+flatpickr('input', options);
+start.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() <= date.getTime()) {
+      window.alert('Please choose a date in the future');
+      setTimer(0, 0, 0, 0);
+      clearInterval(timer);
+    } else {
+      start.disabled = false;
+      newDate = selectedDates[0];
+    }
+  },
+};
+
+const addLeadingZero = value => {
+  return value.padStart(2, '0');
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
