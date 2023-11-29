607a8aae30c63abb269cb0f5fd2bd8d80ba9d76c
Ola Połaniecka <97olka@gmail.com>
2023-11-29 11:10:27 +0100

    java9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,30 +1,39 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
 const delayInput = document.querySelector('[name="delay"]');
 const stepInput = document.querySelector('[name="step"]');
 const amountInput = document.querySelector('[name="amount"]');
 const createBtn = document.querySelector('button');
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    // Reject
-    Notify.failure(`Fulfilled promise ${position} in ${delay}ms`);
-  }
-}
-
 createBtn.addEventListener('submit', e => {
   e.preventDefault();
-  let currentDelay = parseInt(delayInput.value);
   setTimeout(() => {
-    for (let i = 0; i < amountInput.value; i++) {
-      setTimeout(() => {
-        createPromise(i + 1, currentDelay), parseInt(stepInput.value);
-        currentDelay += parseInt(stepInput.value);
-      }, parseInt(stepInput.value) * i);
+    for (let i = 1; i <= amountInput; i++) {
+      const currentDelay = delayInput + (i - 1) * stepInput;
+      createPromise(i, currentDelay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
     }
-  }, parseInt(delayInput.value));
+  });
 });
